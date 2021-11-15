40cabb50dabebeac799cbe1df96ec7ecb902a34e
shaka0184 <shaka0184@gmail.com>
2021-11-15 11:53:42 +0900

    ログ出力用の関数追加

---
+++ pkg/Log/LogUtil.go
@@ -0,0 +1,14 @@
+package Log
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func loggingSettings(filename string) {
+	logfile, _ := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.SetOutput(multiLogFile)
+}
