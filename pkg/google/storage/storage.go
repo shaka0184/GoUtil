8d2923aeadd8955d33584a4c834813705c80f078
shaka0184 <shaka0184@gmail.com>
2022-11-06 23:23:51 +0900

    追加

--- pkg/google/storage/storage.go
+++ pkg/google/storage/storage.go
@@ -1,10 +1,12 @@
 package storage
 
 import (
+	"bufio"
 	"cloud.google.com/go/storage"
 	"context"
 	"github.com/pkg/errors"
 	"io"
+	"os"
 )
 
 func GetReader(ctx context.Context, bucketName, objectPath string) (*storage.Reader, error) {
@@ -36,4 +38,29 @@ func GetByteSlice(ctx context.Context, bucketName, objectPath string) ([]byte, e
 	defer r.Close()
 
 	return res, nil
+}
+
+func Upload(ctx context.Context, bucketName, objectPath string, f *os.File) error {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	obj := client.Bucket(bucketName).Object(objectPath)
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer reader.Close()
+
+	// 書き込み
+	tee := io.TeeReader(reader, f)
+	s := bufio.NewScanner(tee)
+	for s.Scan() {
+	}
+	if err := s.Err(); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
 }
